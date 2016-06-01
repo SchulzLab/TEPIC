25f9ad13d03390e510f92de09408a51a13fc0f04
Fabian Kern <s9fakern@stud.uni-saarland.de>
2016-06-01 09:43:50 +0200

    Improved utils file and implemented one more help function.

--- Code/utils.py
+++ Code/utils.py
@@ -10,15 +10,15 @@ def readGTF(annotationFile):
 		if (len(s) >=9):
 			if (s[2]=="gene" and s[0]!='chrM' and s[0]!='chrX' and s[0]!='chrY'):	
 				if (s[6]=="+"):
-					tss[s[9]]=(s[0].replace("chr",""),int(s[3]))
+					tss[s[9].replace(";","")]=(s[0].replace("chr",""),int(s[3]))
 				else:
-					tss[s[9]]=(s[0].replace("chr",""),int(s[4]))
+					tss[s[9].replace(";","")]=(s[0].replace("chr",""),int(s[4]))
 	gtf.close()
 	return tss
 
 
-# Reads a loop file and returns a dictionary containg all intrachromosomal loops per chromosome in a list:
-# {key : value} -> {chr : [(loopID, start X, end X, start Y, end Y, observations count)]}
+# Reads a loop file and returns a dictionary containing all intrachromosomal loops per chromosome in a list:
+# {key : value} -> {chr : [(loopID, start X, end X, start Y, end Y, observations count, resolution)]}
 def readIntraLoops(loopsFile):
 	lf = open(loopsFile, 'r')
 	loops = {}
@@ -32,7 +32,26 @@ def readIntraLoops(loopsFile):
 		if (len(s) >=8):
 			if(s[0] == s[3] and s[0]!='X' and s[0]!='Y'): # check if loop is intra-chromosomal
 				loopID += 1
-				loops[s[0]].append((loopID, int(s[1]), int(s[2]), int(s[4]), int(s[5]), int(s[7])))
+				resolution = int(s[2]) - int(s[1])
+				loops[s[0]].append((loopID, int(s[1]), int(s[2]), int(s[4]), int(s[5]), int(s[7]), resolution))
+	lf.close()
+	return loops
+
+# Reads a loop file and returns a dictionary containg all intrachromosomal loops accessable by an unique ID:
+# {key : value} -> {loopID : (loopID, start X, end X, start Y, end Y, observations count, resolution)}
+# we keep the loop format for consistent programming and accessing tupel fields
+def readIntraLoopsLookupTable(loopsFile):
+	lf = open(loopsFile, 'r')
+	loops = {}
+	
+	loopID = 0
+	for l in lf:
+		s=l.split()
+		if (len(s) >=8):
+			if(s[0] == s[3] and s[0]!='X' and s[0]!='Y'): # check if loop is intra-chromosomal
+				loopID += 1
+				resolution = int(s[2]) - int(s[1])
+				loops[loopID] = (loopID, int(s[1]), int(s[2]), int(s[4]), int(s[5]), int(s[7]), resolution)
 	lf.close()
 	return loops
 

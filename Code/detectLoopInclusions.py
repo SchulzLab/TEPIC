8de4885647fcce668555970c415ba5907ca0f9cb
Fabian Kern <s9fakern@stud.uni-saarland.de>
2016-06-13 21:45:33 +0200

    Improved detectLoopsInclusions.py and implemented a filter function.

--- Code/detectLoopInclusions.py
+++ Code/detectLoopInclusions.py
@@ -1,6 +1,16 @@
 import argparse
 import utils
 
+def filterInclusions(intraLoops, inclusions):
+	for chrKey in inclusions:
+		chrInclusions = inclusions[chrKey]
+		for inclusion in chrInclusions:
+			if(inclusion[0][6] < inclusion[1][6]):
+				intraLoops[chrKey].remove(inclusion[1])
+			elif(inclusion[0][6] > inclusion[1][6]):
+				intraLoops[chrKey].remove(inclusion[0])
+	return
+
 def run(intraLoops):
 	inclusions = {}
 	for chrKey in intraLoops:
@@ -13,22 +23,15 @@ def run(intraLoops):
 					continue
 				if(loop[6] <= otherloop[6]):# same resolution or lower
 					continue
-				if(loop[1] <= otherloop[1] and otherloop[2] <= loop[2]):
+				if(loop[1] <= otherloop[1] and otherloop[2] <= loop[2] and loop[3] <= otherloop[3] and otherloop[4] <= loop[4]):
 					inclusions[chrKey].append((loop, otherloop))
-				elif(loop[3] <= otherloop[1] and otherloop[2] <= loop[4]):
-					inclusions[chrKey].append((loop, otherloop))
-				elif(loop[1] <= otherloop[3] and otherloop[4] <= loop[2]):
-					inclusions[chrKey].append((loop, otherloop))
-				elif(loop[3] <= otherloop[3] and otherloop[4] <= loop[4]):
-					inclusions[chrKey].append((loop, otherloop))
-					
 	return inclusions
-	
+
 
 def postProcessing(results):
 	header = ''
 	output = ''
-	
+
 	for chrKey in results:
 		chrInclusions = results[chrKey]
 		if(len(chrInclusions) == 0):
@@ -41,22 +44,22 @@ def postProcessing(results):
 				output += str(looppair[0])
 				output += str(looppair[1])
 				output += '\n'
-	
-	utils.writeToFile('detectionoutput.txt', header, output)	
+
+	utils.writeToFile('detectionoutput.txt', header, output)
 	return 0
-	
+
 
 # Main entry point for algorithm, requires:
 # 	a loop-file
 def detectLoopInclusions(loopsFile):
 	print 'Indexing Loops'
 	intraLoops = utils.readIntraLoops(loopsFile)
-	
+
 	print 'Running core algorithm'
 	results = run(intraLoops)
-	
+
 	postProcessing(results)
-	
+
 	return 0
 
 
@@ -66,7 +69,7 @@ def detectLoopInclusions(loopsFile):
 ##
 ######
 
-# preparation-routine 
+# preparation-routine
 parser = argparse.ArgumentParser(description='Checks whether there are (Intrachromosomal) loops of different resolutions in a loop file, so that a loop of a lower resolution contains a loop with a higher resolution.')
 parser.add_argument('loops', help='Path to a loop file')
 

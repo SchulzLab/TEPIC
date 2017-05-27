6bad3ab83fd5903934141d39f29fed061003a307
Fabian Kern <s9fakern@stud.uni-saarland.de>
2017-05-27 18:26:36 +0200

    Implemented Hi-C support in computeRegionFeatures.py. Fixed some minor bugs and added some performance tweaks.

--- Code/computeRegionFeatures.py
+++ Code/computeRegionFeatures.py
@@ -104,9 +104,10 @@ def read_PeakCoverage(filename):
             line = line.split()
             if len(line) < 4:
                 raise ValueError("Error in peak coverage file! Expected: 'chr   start   end coverage' per line.")
-            if line[0] not in peakcoverage:
-                peakcoverage[line[0]] = []
-            peakcoverage[line[0]] += [(int(line[1]), int(line[2]), float(line[3]))]
+            chromosome = line[0].replace("chr", "")
+            if chromosome not in peakcoverage:
+                peakcoverage[chromosome] = []
+            peakcoverage[chromosome] += [(int(line[1]), int(line[2]), float(line[3]))]
     return peakcoverage
 
 
@@ -117,10 +118,34 @@ def read_PeakCoverage_Collection(filename):
             line = line.split()
             if len(line) < 4:
                 raise ValueError("Error in peak coverage file! Expected: 'chr   start   end coverage' per line.")
-            if line[0] not in peakcoverage:
-                peakcoverage[line[0]] = SortedCollection(key=itemgetter(0, 1))
-            peakcoverage[line[0]].insert_right((int(line[1]), int(line[2]), float(line[3])))
+            chromosome = line[0].replace("chr", "")
+            if chromosome not in peakcoverage:
+                peakcoverage[chromosome] = SortedCollection(key=itemgetter(0, 1))
+            peakcoverage[chromosome].insert_right((int(line[1]), int(line[2]), float(line[3])))
     return peakcoverage
+
+
+def read_Intra_Loop_Regions_Collection(filename, resolution_filter):
+    left_region_collection = {}
+    right_region_collection = {}
+    with open(filename) as hi_c_file:
+        # skip header
+        hi_c_file.readline()
+        loopID = 0
+        for line in hi_c_file:
+            line = line.split()
+            if len(line) >= 8:
+                if line[0] == line[3]:  # check if loop is intra-chromosomal
+                    loopID += 1
+                    chromosome = line[0]
+                    resolution = int(line[2]) - int(line[1])
+                    if chromosome not in left_region_collection:
+                        left_region_collection[chromosome] = SortedCollection(key=itemgetter(SORTING_KEY))
+                        right_region_collection[chromosome] = SortedCollection(key=itemgetter(SORTING_KEY))
+                    if resolution_filter is None or resolution_filter == resolution:
+                        left_region_collection[chromosome].insert_right((loopID, int(line[1]), int(line[2]), int(line[4]), int(line[5])))
+                        right_region_collection[chromosome].insert_right((loopID, int(line[4]), int(line[5]), int(line[1]), int(line[2])))
+    return left_region_collection, right_region_collection
 
 
 def sanityCheck(a, b):
@@ -179,9 +204,18 @@ def getRegionsInWindow(annotations, regions_collection, window_size):
             lower_position = annotation[1] - window_size
             upper_position = annotation[1] + window_size
             try:
-                left_item = collection.find_ge(lower_position)
+                left_item = collection.find_lt(lower_position)
             except ValueError:
-                left_item = None
+                try:
+                    left_item = collection.find_ge(lower_position)
+                except ValueError:
+                    left_item = None
+            else:
+                if left_item[SORTING_KEY + 1] < lower_position:
+                    try:
+                        left_item = collection.find_ge(lower_position)
+                    except ValueError:
+                        left_item = None
             try:
                 right_item = collection.find_le(upper_position)
             except ValueError:
@@ -198,28 +232,71 @@ def getRegionsInWindow(annotations, regions_collection, window_size):
     return gene_regions
 
 
-def filterGeneRegions(gene_regions, gene_functions):
+def filterGeneRegions(regions_collection, gene_functions, gene_to_chromosome=None):
     filtered_gene_regions = {}
+    filtered_gene_functions = {}
     for annotation, functional_regions in gene_functions.iteritems():
-        regions = gene_regions[annotation]
-        filtered_gene_regions[annotation] = SortedCollection(key=itemgetter(SORTING_KEY))
-        for functional_region in functional_regions:
-            try:
-                left_boundary = regions.find_lt_index(functional_region[1])
-            except ValueError:
-                try:
-                    left_boundary = regions.find_ge_index(functional_region[1])
-                except ValueError:
-                    left_boundary = len(regions)
-            else:
-                if regions[left_boundary][2] < functional_region[1]:
-                    left_boundary += 1
+        if gene_to_chromosome is None:
+            regions = regions_collection[annotation]
+        else:
+            regions = regions_collection[gene_to_chromosome[annotation[0]]]
+        (filtered_gene_functions[annotation], filtered_gene_regions[annotation]) = get_intersecting_regions(functional_regions, regions)
+    return filtered_gene_regions, filtered_gene_functions
 
-            curr_index = left_boundary
-            while curr_index < len(regions) and regions[curr_index][1] < functional_region[2]:
-                filtered_gene_regions[annotation].insert_right(regions[curr_index])
+
+def get_intersecting_regions(a_regions, b_collection):
+    intersection_a = SortedCollection(key=itemgetter(SORTING_KEY))
+    intersection_b = SortedCollection(key=itemgetter(SORTING_KEY))
+    for a_region in a_regions:
+        try:
+            left_boundary = b_collection.find_lt_index(a_region[1])
+        except ValueError:
+            try:
+                left_boundary = b_collection.find_ge_index(a_region[1])
+            except ValueError:
+                left_boundary = len(b_collection)
+        else:
+            if b_collection[left_boundary][2] < a_region[1]:
+                left_boundary += 1
+
+        curr_index = left_boundary
+        if curr_index < len(b_collection) and b_collection[curr_index][1] <= a_region[2]:
+            intersection_a.insert_right(a_region)
+            while curr_index < len(b_collection) and b_collection[curr_index][1] <= a_region[2]:
+                intersection_b.insert_right(b_collection[curr_index])
                 curr_index += 1
-    return filtered_gene_regions
+    return intersection_a, intersection_b
+
+
+def intersect_regions(a_regions, b_regions_collections):
+    intersecting_regions_a = {}
+    intersecting_regions_b = {}
+    for chromosome, regions in a_regions.iteritems():
+        (intersecting_regions_a[chromosome], intersecting_regions_b[chromosome]) = get_intersecting_regions(regions, b_regions_collections[chromosome])
+    return intersecting_regions_a, intersecting_regions_b
+
+
+def merge_gene_regions(gene_regions, add_gene_regions):
+    for annotation, regions in add_gene_regions.iteritems():
+        if annotation not in gene_regions:
+            gene_regions[annotation] = SortedCollection(key=itemgetter(SORTING_KEY))
+        for region in regions:
+            try:
+                gene_regions[annotation].find(region[SORTING_KEY])
+            except ValueError:
+                pass
+            else:
+                gene_regions[annotation].insert_right(region)
+    return gene_regions
+
+
+def get_complement_gene_loop_regions(gene_loops):
+    complement_gene_loops = {}
+    for annotation, regions in gene_loops.iteritems:
+        complement_gene_loops[annotation] = SortedCollection(key=itemgetter(SORTING_KEY))
+        for region in regions:
+            complement_gene_loops.insert_right(region[0], region[3], region[4], region[1], region[2])
+    return complement_gene_loops
 
 
 def computeRegionFeatures(gene_regions, peakcoverage_collection, gene_to_chromosome, decay):
@@ -265,13 +342,16 @@ def main():
     parser.add_argument("gtf", help="Genome annotation file")
     parser.add_argument("regions", help="Region file")
     parser.add_argument("peakcoverage", help="Region coverage file.")
-    parser.add_argument("--functional_regions", nargs="?", help="Enhancer/Promoter Region file")
+    parser.add_argument("--functional_regions", nargs="?", default=None, help="Enhancer/Promoter Region file")
     parser.add_argument("--window_size", nargs="?", type=int, help="Indicates whether to use a window of the given size for gene-region assignment. If no size is given, a nearest neighbor assignment is computed.")
     parser.add_argument("--outputprefix", nargs="?", default="", const="", help="Prefix to be used for output file(s).")
     decay_parser = parser.add_mutually_exclusive_group(required=True)
     decay_parser.add_argument('--decay', dest='decay', action='store_true', help="Flag option. If set an exponential decay function is used to weight the features. Default behavior is False.")
     decay_parser.add_argument('--no-decay', dest='decay', action='store_false')
     parser.set_defaults(decay=True)
+    parser.add_argument("--hi_c_regions", nargs="?", default=None, help="If the name of the Hi-C loop file is provided, all regions will be intersected with annotated Hi-C (intrachromosomal) loop regions around the TSS of each annotated gene.")
+    parser.add_argument("--loopwindows", nargs="?", default=25000, type=int, help="Defines the window-size around the TSS in which all loops are considered for intersecting with openChromatin regions.")
+    parser.add_argument("--resolution", nargs="?", default=5000, help="Defines the Hi-C resolution of the loops which should be considered.")
     args = parser.parse_args()
 
     print "Loading files ..."
@@ -296,11 +376,11 @@ def main():
 
     print "Ready!"
     print "Starting annotation..."
-    print "Using window size: " + str(args.window_size)
+    print "Using window size: " + str(args.window_size/2)
     print "Decay set to: " + str(args.decay)
 
     if args.window_size is not None:
-        gene_regions = getRegionsInWindow(annotations, regions_collection, args.window_size)
+        gene_regions = getRegionsInWindow(annotations, regions_collection, args.window_size/2)
         print "Assigned regions to " + str(len(gene_regions)) + " genes"
         count = 0
         for _, regs in gene_regions.iteritems():
@@ -314,25 +394,58 @@ def main():
             count += len(regs)
         print "Total number of assigned regions: " + str(count)
 
-    if args.functional_regions is not None:
+    if args.functional_regions is not None and args.hi_c_regions is None:
         print "Loading functional regions..."
         if args.window_size is not None:
             functional_regions_collection = read_Regions_Collection(args.functional_regions)
             sanityCheck(functional_regions_collection, annotations)
-            gene_functions = getRegionsInWindow(annotations, functional_regions_collection, args.window_size)
+            gene_functions = getRegionsInWindow(annotations, functional_regions_collection, args.window_size/2)
         else:
             functional_regions = read_Regions(args.functional_regions)
             sanityCheck(annotations_collection, functional_regions)
             gene_functions = getNearestNeighbors(annotations_collection, functional_regions)
         sanityCheck(gene_regions, gene_functions)
         print "Filtering regions using functional annotations..."
-        gene_regions = filterGeneRegions(gene_regions, gene_functions)
+        (gene_regions, _) = filterGeneRegions(gene_regions, gene_functions)
         print "Assigned regions to " + str(len(gene_regions)) + " genes"
         count = 0
         for _, regs in gene_regions.iteritems():
             count += len(regs)
         print "Total number of assigned regions: " + str(count)
 
+    elif args.hi_c_regions is not None:
+        print "Running into Hi-c mode.."
+        print "Using loop-window size: " + str(args.loopwindows / 2)
+        # read hi-c file and keep loops with desired resolution
+        resolution = None
+        if args.resolution is not None:
+            resolution = int(args.resolution)
+        (regions_left_collection, regions_right_collection) = read_Intra_Loop_Regions_Collection(args.hi_c_regions, resolution)
+        sanityCheck(regions_left_collection, annotations)
+        sanityCheck(regions_right_collection, annotations)
+        # get gene-wise hi-c loops in loopwindows
+        gene_loops_left = getRegionsInWindow(annotations, regions_left_collection, args.loopwindows/2)
+        gene_loops_right = getRegionsInWindow(annotations, regions_right_collection, args.loopwindows/2)
+        # filter regions that are in these gene-wise hi-c regions
+        (gene_regions_left, _) = filterGeneRegions(regions_collection, gene_loops_left, gene_to_chromosome)
+        (gene_regions_right, _) = filterGeneRegions(regions_collection, gene_loops_right, gene_to_chromosome)
+        if args.functional_regions is not None:
+            print "Using also enhancer regions..."
+            # !use regions in tss window + intersect functional regions with hi-c regions and intersect those again with regions!
+            # read functional regions
+            functional_regions_collection = read_Regions_Collection(args.functional_regions)
+            sanityCheck(functional_regions_collection, annotations)
+            # filter the regions in gene-wise hi-c regions further using the functional regions
+            (_, gene_regions_left) = filterGeneRegions(functional_regions_collection, gene_regions_left, gene_to_chromosome)
+            (_, gene_regions_right) = filterGeneRegions(functional_regions_collection, gene_regions_right, gene_to_chromosome)
+        # OR: !just use regions in tss window and those in hi-c regions!
+        # add remaining regions to the gene-regions
+        gene_regions = merge_gene_regions(merge_gene_regions(gene_regions, gene_regions_left), gene_regions_right)
+        print "Assigned regions to " + str(len(gene_regions)) + " genes"
+        count = 0
+        for _, regs in gene_regions.iteritems():
+            count += len(regs)
+        print "Total number of assigned regions: " + str(count)
     feature_matrix = computeRegionFeatures(gene_regions, peakcoverage_collection, gene_to_chromosome, args.decay)
     writeGeneFeatureMatrix(args.outputprefix + "RegionFeatures.txt", feature_matrix)
 
